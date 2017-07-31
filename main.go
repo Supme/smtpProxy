044d23a98b85b1b161b3cebb204dc3de29aea2ea
supme <supmea@gmail.com>
2017-07-31 12:05:14 +0300

    use external lib for send

--- main.go
+++ main.go
@@ -6,14 +6,10 @@ import (
 	"os"
 	"log"
 	"io"
-	"net/smtp"
-	"strings"
 	"fmt"
-	"golang.org/x/net/idna"
-	"errors"
-	"time"
 	"github.com/BurntSushi/toml"
 	"path/filepath"
+	"github.com/supme/directEmail"
 )
 
 var config struct{
@@ -154,89 +150,100 @@ func connection(conn net.Conn, lg io.Writer) {
 }
 
 func (msg *message) send() error {
-
-	var myGlobalIP string
-	myIp,_, err := net.SplitHostPort(msg.ip.String())
-	myGlobalIP, ok := config.MapIp[myIp]
-	if !ok {
-		myGlobalIP = myIp
-	} else if config.Debug {
-		mlog.Printf("Local IP address '%s' change to '%s'\n", myIp, myGlobalIP)
-	}
-
-	name, err := net.LookupAddr(myGlobalIP)
-	if err != nil && len(name) < 1 {
-		return err
-	}
-
-	splitEmail := strings.SplitN(msg.rcptTo, "@", 2)
-	if len(splitEmail) != 2 {
-		return errors.New("550 Bad email")
-	}
-
-	domain, err := idna.ToASCII(splitEmail[1])
-	if err != nil {
-		return errors.New(fmt.Sprintf("550 Domain name failed: %v", err))
-	}
-
-	addr := &net.TCPAddr{
-		IP: net.ParseIP(msg.ip.String()),
-	}
-	iface := net.Dialer{LocalAddr: addr}
-
-	record, err := net.LookupMX(domain)
-	if err != nil {
-		return errors.New(fmt.Sprintf("550 %v", err))
-	}
-
-	var (
-		conn net.Conn
-		server string
-	)
-	for i := range record {
-		server = strings.TrimRight(strings.TrimSpace(record[i].Host), ".")
-		conn, err = iface.Dial("tcp", net.JoinHostPort(server, "25"))
-		if err == nil {
-			break
-		}
-	}
-	if err != nil {
-		return errors.New(fmt.Sprintf("550 %v", err))
-	}
-	conn.SetDeadline(time.Now().Add(5 * time.Minute))
-
-	c, err := smtp.NewClient(conn, server)
-	if err != nil {
-		return err
-	}
-
-	if err := c.Hello(strings.TrimRight(name[0], ".")); err != nil {
-		return err
-	}
-
-	if err := c.Mail(msg.mailFrom); err != nil {
-		return err
-	}
-
-	if err := c.Rcpt(msg.rcptTo); err != nil {
-		return err
-	}
-
-	w, err := c.Data()
-	if err != nil {
-		return err
-	}
-
-	_, err = fmt.Fprint(w, msg.data)
-	if err != nil {
-		return err
-	}
-
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-
-	return c.Quit()
-
+	email := directEmail.New()
+	email.Ip = msg.ip.String()
+	email.MapIp = config.MapIp
+	email.FromEmail = msg.mailFrom
+	email.ToEmail = msg.rcptTo
+	email.MapIp = config.MapIp
+	email.SetRawMessageString(msg.data)
+	return email.Send()
 }
+
+//func (msg *message) send() error {
+//
+//	var myGlobalIP string
+//	myIp,_, err := net.SplitHostPort(msg.ip.String())
+//	myGlobalIP, ok := config.MapIp[myIp]
+//	if !ok {
+//		myGlobalIP = myIp
+//	} else if config.Debug {
+//		mlog.Printf("Local IP address '%s' change to '%s'\n", myIp, myGlobalIP)
+//	}
+//
+//	name, err := net.LookupAddr(myGlobalIP)
+//	if err != nil && len(name) < 1 {
+//		return err
+//	}
+//
+//	splitEmail := strings.SplitN(msg.rcptTo, "@", 2)
+//	if len(splitEmail) != 2 {
+//		return errors.New("550 Bad email")
+//	}
+//
+//	domain, err := idna.ToASCII(splitEmail[1])
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("550 Domain name failed: %v", err))
+//	}
+//
+//	addr := &net.TCPAddr{
+//		IP: net.ParseIP(msg.ip.String()),
+//	}
+//	iface := net.Dialer{LocalAddr: addr}
+//
+//	record, err := net.LookupMX(domain)
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("550 %v", err))
+//	}
+//
+//	var (
+//		conn net.Conn
+//		server string
+//	)
+//	for i := range record {
+//		server = strings.TrimRight(strings.TrimSpace(record[i].Host), ".")
+//		conn, err = iface.Dial("tcp", net.JoinHostPort(server, "25"))
+//		if err == nil {
+//			break
+//		}
+//	}
+//	if err != nil {
+//		return errors.New(fmt.Sprintf("550 %v", err))
+//	}
+//	conn.SetDeadline(time.Now().Add(5 * time.Minute))
+//
+//	c, err := smtp.NewClient(conn, server)
+//	if err != nil {
+//		return err
+//	}
+//
+//	if err := c.Hello(strings.TrimRight(name[0], ".")); err != nil {
+//		return err
+//	}
+//
+//	if err := c.Mail(msg.mailFrom); err != nil {
+//		return err
+//	}
+//
+//	if err := c.Rcpt(msg.rcptTo); err != nil {
+//		return err
+//	}
+//
+//	w, err := c.Data()
+//	if err != nil {
+//		return err
+//	}
+//
+//	_, err = fmt.Fprint(w, msg.data)
+//	if err != nil {
+//		return err
+//	}
+//
+//	err = w.Close()
+//	if err != nil {
+//		return err
+//	}
+//
+//	return c.Quit()
+//
+//}
